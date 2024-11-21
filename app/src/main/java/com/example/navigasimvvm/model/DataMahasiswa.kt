fe248412ff10ffe619597f85186a1cd0b50649f3
yafidaffaa <yafi.d.andriansyah@gmail.com>
2024-11-21 20:06:13 +0700

    menambahkan variable damlam data class DataMahasiswa

--- app/src/main/java/com/example/navigasimvvm/model/DataMahasiswa.kt
+++ app/src/main/java/com/example/navigasimvvm/model/DataMahasiswa.kt
@@ -2,6 +2,9 @@ package com.example.navigasimvvm.model
 
 data class DataMahasiswa(
     val nama: String = "",
+    val nim: String = "",
     val gender: String = "",
     val alamat: String = "",
+    val nohp: String = "",
+    val email: String = "",
 )
